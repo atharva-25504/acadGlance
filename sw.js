7badca1e6a96916c92ad30f08e0c55bd3b86755c
atharva-25504 <atharva.p25504@iimtrichy.ac.in>
2026-09-07 21:46:32 +0530

    bump cache version to evict stale shell after data-model fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'acadglance-v1';
+const CACHE = 'acadglance-v2';
 const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
