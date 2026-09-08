6171f24abbd0efb6ad4165cb00a6c46edcc576f0
atharva-25504 <atharva.p25504@iimtrichy.ac.in>
2026-09-08 09:22:28 +0530

    bump cache to v3 after term-day computation fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'acadglance-v2';
+const CACHE = 'acadglance-v3';
 const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
